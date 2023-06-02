c77830aa47d50a2ee8e3197019c8d1e6d2b5c9fe
Allan Jardine <allan.jardine@sprymedia.co.uk>
2023-06-02 15:11:13 +0100

    1be9c134e5733f639465d2a2b14203c1ddb2a317 New: Dark mode support for DataTables and Bootstrap 5 styling
    Sync to source repo @1be9c134e5733f639465d2a2b14203c1ddb2a317

--- js/colReorder.semanticui.min.mjs
+++ js/colReorder.semanticui.min.mjs
@@ -1,4 +1,4 @@
 /*! Semanic UI styling wrapper for ColReorder
  * © SpryMedia Ltd - datatables.net/license
  */
-import $ from"jquery";import DataTable from"datatables.net-se";import ColReorder from"datatables.net-colreorder";export default DataTable;
+import jQuery from"jquery";import DataTable from"datatables.net-se";import ColReorder from"datatables.net-colreorder";let $=jQuery;export default DataTable;
